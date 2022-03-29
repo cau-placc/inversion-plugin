dac92dc436eb7addcc82b2e06119fe19ed603cde
Kai Prott <kai.prott@hotmail.de>
2022-03-29 12:15:40 +0200

    Add Peano example that was bugged

--- sandbox/src/Peano.hs
+++ sandbox/src/Peano.hs
@@ -22,3 +22,12 @@ add (S m) n = S (add m n)
 lengthPeano :: [a] -> Peano
 lengthPeano [] = Z
 lengthPeano (_:xs) = S (lengthPeano xs)
+
+replicateP :: Peano -> a -> [a]
+replicateP Z _ = []
+replicateP (S n) x = x : replicateP n x
+
+unrle3P :: Eq a => [(a, Peano)] -> [a]
+unrle3P [] = []
+unrle3P [(x, S n)] = replicateP (S n) x
+unrle3P ((x, S n) : (y, m) : ts) | y /= x = replicateP (S n) x ++ unrle3P ((y, m) : ts)
