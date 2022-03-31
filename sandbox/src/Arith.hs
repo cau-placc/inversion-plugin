b006b6b9898563802a29e72e407ec39f260f197c
Finn Teegen <fte@informatik.uni-kiel.de>
2022-03-31 16:03:20 +0200

    Clean sandbox and update cabal file

--- sandbox/src/Arith.hs
+++ sandbox/src/Arith.hs
@@ -2,6 +2,14 @@
 
 module Arith where
 
+myNot :: Bool -> Bool
+myNot False = True
+myNot True  = False
+
+formel x y z = x && y || z
+--TODO: schaltkreis beispiel zur analyse
+-- TODO: fulladder
+
 con :: Int
 con = 42
 
@@ -11,8 +19,43 @@ con2 () = 42
 plus :: Num a => a -> a -> a
 plus = (+)
 
+minus :: Num a => a -> a -> a
+minus = (-)
+
+leq :: Ord a => a -> a -> Bool
+leq = (<=)
+
+geq :: Ord a => a -> a -> Bool
+geq = (>=)
+
+charTest :: Char -> Bool
+charTest '\\' = True
+charTest _   = False
+
+charTest2 :: Char -> Bool
+charTest2 _ = True
+
+stringTest :: String -> Bool
+stringTest "a" = True
+stringTest _   = False
+
+myCompare :: Ord a => a -> a -> Ordering
+myCompare = compare
+
+mySignum :: Num a => a -> a
+mySignum = signum
+
 someArith :: Num a => a -> a -> a
 someArith x y = x * 42 + y -- take 30 $ $(inv 'someArith) 42 -- 16s
+
+someArith2 :: Num a => a -> a -> a
+someArith2 x y = (x * x) * 42 + y
+
+
+
+
+--testf :: a -> b -> c
+--testf x y = z where z = z
 
 ack, ackG :: (Ord a, Num a) => a -> a -> a
 ack 0 n = n + 1
@@ -36,15 +79,10 @@ cantor x y | x >= 0 && y >= 0 = ((x + y) * (x + y + 1) `div` 2) + y -- $(inv 'ca
 -- ^ 1 res immediately, but takes a loooooooooong time to terminate
 
 
-
-
-
-
-
-
-
-
 -- 0 == (V -1)
 -- (V 0) == (V -2) + 1
 
 -- ack (v -1) (v -2) = 1
+
+testf :: a -> b -> c
+testf x y = let z = z in z
