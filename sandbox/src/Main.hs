c5ce60e54c111d52c1bc40f9fd00ca13d6474e9e
Finn Teegen <fte@informatik.uni-kiel.de>
2022-04-05 14:20:33 +0200

    Update Main module of sandbox

--- sandbox/src/Main.hs
+++ sandbox/src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, FlexibleContexts, ViewPatterns #-}
+{-# LANGUAGE TemplateHaskell, FlexibleContexts, ViewPatterns, PartialTypeSignatures #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 module Main where
 
@@ -6,33 +6,38 @@ import Plugin.InversionPlugin
 
 import TreeTraversal
 
-f = map showFree $ ($(inOutClassInv 'id False [[| Test True True |]] [| var 2 |]) :: [Solo Test])
+main :: IO ()
+main = return ()
 
-{-
-import Prelude hiding (map, lookup, (++), last, Maybe(..))
+--f = map showFree $ ($(inOutClassInv 'id False [[| Test True True |]] [| var 2 |]) :: [Solo Test])
+
+split :: _ => _
+split = $(inv '(++) True)
 
 split123 :: [([Int], [Int])]
-split123 = $(inv '(++)) [1,2,3]
+split123 = split [1,2,3]
 
-anythingBut42 :: [Int]
-anythingBut42 = $(inv 'is42) False
+--anythingBut42 :: [Int]
+--anythingBut42 = $(inv 'is42) False
 
-last :: Invertible a => [a] -> a
-last $(funPat '(++) [p| _ |] [p| [x] |]) = x
-last _ = error "empty list"
+--last :: Invertible a => [a] -> a
+--last $(funPat '(++) [p| _ |] [p| [x] |]) = x
+--last _ = error "empty list"
 
-reverseLookup :: (Invertible a, Invertible b, Eq a, Lifted (Eq a))
-              => [(a, b)] -> Maybe b -> [a]
-reverseLookup = $(partialInv 'lookup [2])
+--TODO: impossible to give type signature as FL is not exported (and should not be)
+--TODO: Maybe use "Inverted" = "Lifted FL"
+--reverseLookup :: (Invertible a, Invertible b, Eq a, Lifted FL (Eq a))
+--              => [(a, b)] -> Maybe b -> [a]
+reverseLookup :: _ => _
+reverseLookup = $(partialInv 'lookup True [1])
 
 testMap :: [(Int, Maybe Bool)]
 testMap = [(0, Nothing), (2, Just False)]
 
-hasDuplicates :: Invertible a => [a] -> Bool
-hasDuplicates $(funPat 'hasDuplicatesPat [p| _ |] [p| _ |] [p| _ |] [p| _ |]) = True
-hasDuplicates _ = False
+--hasDuplicates :: Invertible a => [a] -> Bool
+--hasDuplicates $(funPat 'hasDuplicatesPat [p| _ |] [p| _ |] [p| _ |] [p| _ |]) = True
+--hasDuplicates _ = False
 
-isSame :: Invertible a => (a, a) -> Bool
-isSame $(funPat 'dup [p| _ |]) = True
-isSame _                       = False
--}
+--isSame :: Invertible a => (a, a) -> Bool
+--isSame $(funPat 'dup [p| _ |]) = True
+--isSame _                       = False
