e3776ce2a74f430d917173491605171e1feeb748
Finn Teegen <fte@informatik.uni-kiel.de>
2022-04-07 11:26:31 +0200

    Update run-length encoding example

--- sandbox/src/RunLength.hs
+++ sandbox/src/RunLength.hs
@@ -2,27 +2,31 @@
 
 module RunLength where
 
-import Peano
+import P
 
-rleNaiveOnePass :: Eq a => [a] -> [(a, Int)]
-rleNaiveOnePass = foldr rle' []
-  where rle' c []                        = [(c, 1)]
-        rle' c ((x, n) : ts) | c == x    = (x, n + 1) : ts
-                             | otherwise = (c, 1) : (x, n) : ts
+rle :: Eq a => [a] -> [(a, Int)]
+rle = foldr rle' []
+  where rle' c ((x, n) : ts) | c == x = (x, n + 1) : ts
+        rle' c ts                     = (c, 1) : ts
 
-rleNaive :: Eq a => [a] -> [(a, Int)]
-rleNaive [] = []
-rleNaive (c:cs) = let l = length (takeWhile (== c) cs)
-                  in (c, l + 1) : rleNaive (drop l cs)
+rle2 :: Eq a => [a] -> [(a, Int)]
+rle2 [] = []
+rle2 (c:cs) = let l = length (takeWhile (== c) cs)
+              in (c, l + 1) : rle2 (drop l cs)
 
---TODO: Better variant
+rle3 :: Eq a => [a] -> [(a, Int)]
+rle3 [] = []
+rle3 (c:cs) = let l = length (takeWhile (== c) cs)
+              in (c, l + 1) : rle3 (dropWhile (== c) cs)
+
+--TODO: Better variant, that terminates?
 
 rleP :: Eq a => [a] -> [(a, P)]
 rleP [] = []
 rleP (c:cs) = let l = lengthP (takeWhile (== c) cs)
               in (c, S l) : rleP (dropP l cs)
 
--- The inverse of `unrleNaive` is not the "right" one as it computes many run-length encodings that have too short sequences in it (or even sequences of length 0).
+-- The inverses of `unrleNaive` and `unrleNaiveButBetter`are not the "right" ones as they computes run-length encodings that have too short sequences in it (or even sequences of length 0 in case of `unrleNaive`).
 
 unrleNaive :: [(a, Int)] -> [a]
 unrleNaive = concatMap (uncurry (flip replicate))
@@ -31,11 +35,13 @@ unrleNaiveButBetter :: [(a, Int)] -> [a]
 unrleNaiveButBetter [] = []
 unrleNaiveButBetter ((x, n) : ts) | n > 0 = replicate n x ++ unrleNaiveButBetter ts
 
+-- The following correct inverse of `rle` does not terminate.
 unrle :: Eq a => [(a, Int)] -> [a]
 unrle [] = []
 unrle ((x, n) : []) | n > 0 = replicate n x
 unrle ((x, n) : (y, m) : ts) | y /= x && n > 0 = replicate n x ++ unrle ((y, m) : ts)
 
+-- However, using Peano numbers helps w.r.t. termination.
 unrleP :: Eq a => [(a, P)] -> [a]
 unrleP [] = []
 unrleP ((x, n@(S _)) : []) = replicateP n x
