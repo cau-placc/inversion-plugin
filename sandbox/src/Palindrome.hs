79c6c16ad5c19ca8c8442f0e11fdbc69734bc6eb
Finn Teegen <fte@informatik.uni-kiel.de>
2022-04-07 11:25:41 +0200

    Update palindrome example

--- sandbox/src/Palindrome.hs
+++ sandbox/src/Palindrome.hs
@@ -1,20 +1,13 @@
 {-# OPTIONS_GHC -fplugin Plugin.InversionPlugin #-}
 
--- TODO: Taken from "Principles of Inverse Computation and the Universal Resolving Algorithm" and "Rebuilding a Tree from Its Traversals: A Case Study of Program Inversion"
-
 module Palindrome where
 
---TODO: mkPalindrome ::
-
-fromPalindromes :: Eq a => [[a]] -> [a]
-fromPalindromes xs | all2 (\x -> notNull2 x && isPalindrome x) xs = concat2 xs
+mkPalindrome :: [a] -> Maybe a -> [a]
+mkPalindrome xs Nothing  = xs ++ reverse xs
+mkPalindrome xs (Just x) = xs ++ [x] ++ reverse xs
 
 isPalindrome :: Eq a => [a] -> Bool
-isPalindrome xs = xs == reverse2 xs
+isPalindrome xs = xs == reverse xs
 
---[a] -> [[a]]
-fromPalindromesL :: Eq a => [(P, [a])] -> (P, [a])
-fromPalindromesL xs | all2 (\(n, x) -> notNull2 x && isPalindromeL n x) xs = concat2L xs
-
-isPalindromeL :: Eq a => P -> [a] -> Bool
-isPalindromeL n xs = xs == reverseL n xs
+fromPalindromes :: Eq a => [[a]] -> [a]
+fromPalindromes xs | and (map (\x -> not (null x) && isPalindrome x) xs) = concat xs
