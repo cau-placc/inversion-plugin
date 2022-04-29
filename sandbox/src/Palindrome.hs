09bb1b1bae18f276396582440a449122d094e214
Finn Teegen <fte@informatik.uni-kiel.de>
2022-04-29 13:20:39 +0200

    Extend palindrome example to yield a single solution

--- sandbox/src/Palindrome.hs
+++ sandbox/src/Palindrome.hs
@@ -11,3 +11,10 @@ isPalindrome xs = xs == reverse xs
 
 fromPalindromes :: Eq a => [[a]] -> [a]
 fromPalindromes xs | and (map (\x -> not (null x) && isPalindrome x) xs) = concat xs
+
+-- TODO: Testen, ob das nachfolgende Palindrom nicht dasselbe ist, um nur die längsten zu bekommen
+fromPalindromes2 :: Eq a => [[a]] -> [a]
+fromPalindromes2 xs | and (map (\x -> not (null x) && isPalindrome x) xs) && check xs = concat xs
+  where check [] = True
+        check [_] = True
+        check (x:y:xs) = x /= y && check (y:xs)
