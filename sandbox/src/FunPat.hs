c570e58c682003c341a8b5489f8c185276e8d34b
Finn Teegen <fte@informatik.uni-kiel.de>
2022-05-09 14:30:34 +0200

    Add functional pattern examples

--- sandbox/src/FunPat.hs
+++ sandbox/src/FunPat.hs
@@ -1,7 +1,10 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ViewPatterns     #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE ViewPatterns          #-}
 
 module FunPat where
+
+import FunPatSrc
 
 import Plugin.InversionPlugin
 
@@ -12,3 +15,19 @@ lastVP (\arg -> [p | p@(_, [_]) <- $(inv '(++) False) arg] -> ((_, [x]) : _)) = 
 lastVP2 (\arg -> [p | p@(_, [_]) <- $(inv '(++) True) arg] -> ((_, [x]) : _)) = x
 
 lastIC ($(inClassInv '(++) False [[| var 0 |], [| [var 1] |]]) -> ((_, x) : _)) = x
+
+h $(funPat 'pat [p| y |]) = True
+
+--lazyUnifyFL (x, empty) (y,y)
+-- (y,y) =:<= (x, failed)
+-- f (y, y)
+-- f (x, y) | x =:= y
+-- (x, z) =:<= (y, y)
+testFunPat :: _ => Bool -> _
+testFunPat = \x -> $(inOutClassInv 'g True [[| var 0 |], [| var 0 |]] [| (var 1, x) |])
+
+testFunPat2 :: _ => Bool -> _
+testFunPat2 = \x -> $(inOutClassInv 'g True [[| var 0 |], [| x |]] [| (var 1, var 1) |])
+
+testFunPat3 :: _ => Bool -> _
+testFunPat3 = \x -> $(inOutClassInv 'g True [[| var 0 |], [| var 0 |]] [| (var 0, x) |])
