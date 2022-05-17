f6a3fb5d5427fbb705f9eedc4a32788989a787b4
Finn Teegen <fte@informatik.uni-kiel.de>
2022-05-17 13:36:22 +0200

    Fix core usage in Eden library port

--- sandbox/src/Eden/ParPrimConcHs.hs
+++ sandbox/src/Eden/ParPrimConcHs.hs
@@ -4,7 +4,7 @@
 -- Module      :  Control.Parallel.Eden.ParPrimConcHs
 -- Copyright   :  (c) Philipps Universitaet Marburg 2005-2010
 -- License     :  BSD-style (see the file LICENSE)
--- 
+--
 -- Maintainer  :  eden@mathematik.uni-marburg.de
 -- Stability   :  beta
 -- Portability :  not portable
@@ -16,7 +16,7 @@
 -- Depends on GHC.
 --
 -- Eden Group Marburg ( http:\/\/www.mathematik.uni-marburg.de/~eden )
--- 
+--
 -----------------------------------------------
 
 
@@ -27,14 +27,14 @@ module Eden.ParPrimConcHs
      , createC        -- creating placeholders :: IO (ChanName' a, a)
      , connectToPort  -- set thread's receiver :: ChanName' a -> IO ()
      , sendData       -- sending data to recv. :: Mode -> a -> IO ()
-     , Mode(..)       -- send modes:  implemented: 
+     , Mode(..)       -- send modes:  implemented:
                       --      1 - connect (no graph needed)
                       --      2 - stream  (list element)
                       --      3 - single  (single value)
                       --      4 - rFork   (receiver creates a thread, different ports)
                       -- additional payload (currently only for rFork) in high bits
      , simInitPes
-         ) 
+         )
    where
 
 import GHC.Base(unsafeCoerce# )
@@ -85,7 +85,7 @@ fromDyn e (Untyped x) = maybe e id $ cast x
 
 ---- Simulation specials ----
 
--- global ID supply for process IDs and Channel IDs: 
+-- global ID supply for process IDs and Channel IDs:
 --    (CAF trick, evaluated, i.e. created, by first usage)
 {-# NOINLINE idSupply #-}
 idSupply :: MVar Int
@@ -100,28 +100,28 @@ freshId = do i <- takeMVar idSupply
 -- process and thread book-keeping:
 
 -- (PE, processID, Maybe connected channel)
-type ThreadInfo = (Int,Int,Maybe Int) 
+type ThreadInfo = (Int,Int,Maybe Int)
 
 -- global thread table: ID -> ThreadInfo
 --    (first time called: created with first thread as an entry)
 {-# NOINLINE thrs #-}
 thrs :: MVar (Map ThreadId (Int,Int,Maybe Int))
-thrs = unsafePerformIO (myThreadId >>= \id -> 
+thrs = unsafePerformIO (myThreadId >>= \id ->
             newMVar (Map.insert id (1,1,Nothing) Map.empty ))
 
 -- retrieving own thread information
 myInfo :: IO ThreadInfo
-myInfo = do tid        <- myThreadId 
+myInfo = do tid        <- myThreadId
             thrMap     <- readMVar thrs
-            case Map.lookup tid thrMap of 
+            case Map.lookup tid thrMap of
                Nothing -> error (show tid ++ " not found!")
                Just x  -> return x
 
 -- retrieving the channel a thread has connected to
 myChan :: IO Int
-myChan = do (_,_,c) <- myInfo 
-            case c of 
-              Nothing -> do tid <- myThreadId 
+myChan = do (_,_,c) <- myInfo
+            case c of
+              Nothing -> do tid <- myThreadId
                             error (show tid ++ " not connected!")
               Just x  -> return x
 
@@ -143,36 +143,36 @@ chs = unsafePerformIO (newMVar Map.empty)
 
 -- for Connect messages: only register the calling thread as the sender
 registerSender :: Int -> IO ()
-registerSender id 
+registerSender id
     = do cMap <- takeMVar chs
          tid  <- myThreadId
          case Map.lookup id cMap of
             Nothing      -> error $ "missing MVar for Id " ++ show id
             Just (t,var) -> if (t == Nothing || t == Just tid)
-                     then do putMVar chs 
+                     then do putMVar chs
                                  (Map.insert id (Just tid,var) cMap)
                      else error ("duplicate connect message: "
-                     ++ show tid ++ "->" 
+                     ++ show tid ++ "->"
                      ++ show id)
 
 -- for receiving messages, removes the channel (Data message)
 getRemoveCVar :: Int -> IO (MVar Untyped)
 getRemoveCVar id = do cMap <- takeMVar chs
                       case Map.lookup id cMap of
-                        Nothing       -> error ("missing MVar for Id " 
+                        Nothing       -> error ("missing MVar for Id "
                                              ++ show id)
                         Just (_,var)  -> do putMVar chs (Map.delete id cMap)
                                             return var
 
 -- for receiving stream messages, updates the channel, checks the sender
 updateGetCVar :: MVar Untyped -> Int -> IO (MVar Untyped )
-updateGetCVar newVar id 
+updateGetCVar newVar id
     = do cMap <- takeMVar chs
          tid  <- myThreadId
          case Map.lookup id cMap of
             Nothing      -> error $ "missing MVar for Id " ++ show id
-            Just (t,var) -> if (t == Nothing || t == Just tid) 
-                  then do putMVar chs 
+            Just (t,var) -> if (t == Nothing || t == Just tid)
+                  then do putMVar chs
                               (Map.insert id (Just tid,newVar) cMap)
                           return var
                   else error "1:1 restriction violated"
@@ -182,9 +182,9 @@ updateGetCVar newVar id
 pesVar :: MVar ([Int],())
 pesVar = unsafePerformIO (newMVar (placementList,()))
     where placementList = leftrotate peNums
-          peNums = if numCapabilities == 1 
-                       then [1..4]  -- arbitrary default: 4 PEs
-                       else [1..numCapabilities]
+          peNums = [1..numCapabilities] --if numCapabilities == 1
+                       --then [1..4]  -- arbitrary default: 4 PEs
+                       --else [1..numCapabilities]
 
 leftrotate :: [a] -> [a]
 leftrotate [] = []
@@ -195,7 +195,7 @@ simInitPes pes | pes < 1 = error "invalid number of PEs requested"
                | otherwise = do  (_,test) <- takeMVar pesVar
                                  trace ("Init. with " ++ show pes ++ " PEs.")
                                  test `seq` -- protect against double init.
-                                   putMVar pesVar 
+                                   putMVar pesVar
                                      ([2..pes+1],error "double simInitPes")
 
 
@@ -229,7 +229,7 @@ selfPe = do (pe,_,_) <- myInfo
             return pe
 
 -- abstract outside!
-data ChanName' a = Chan Int Int Int 
+data ChanName' a = Chan Int Int Int
    deriving (Show, Generic)
 
 -- tweaking fork primop from concurrent haskell... (not returning threadID)
@@ -246,7 +246,7 @@ fork action = do  (pe,p,_) <- myInfo
                         trace "TODO00"
                         action
                         trace "TODO1"
-                        removeThread id 
+                        removeThread id
 
 -- creation of one placeholder and one new inport
 -- returns consistent channel type (channel of same type as data)
@@ -294,10 +294,10 @@ sendData Stream d = do cd <- myChan
                            newList = d: fromDyn undefined x
                        putMVar var $ toDyn newList
 
-sendData (Instantiate maybePe) d 
+sendData (Instantiate maybePe) d
          = do  newPid <- freshId
                pes <- noPe
-               pe <- if maybePe == 0 then choosePe 
+               pe <- if maybePe == 0 then choosePe
                                      else return (1+((maybePe-1) `mod` pes))
                trace ("new process on PE " ++ show pe)
                tlist <- takeMVar thrs
