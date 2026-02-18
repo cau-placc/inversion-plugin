fb4db2ac45b28d0c6913efff759cbc4a9bba65e3
Finn Teegen <finnteegen@googlemail.com>
2026-02-18 15:04:19 +0100

    Fix functional pattern example
    Fixes #5

--- sandbox/src/FunctionalPatterns.hs
+++ sandbox/src/FunctionalPatterns.hs
@@ -10,8 +10,8 @@ import ToBeInverted
 -- used in the functional pattern.
 -- The second argument is a list of quasi-quoted patterns,
 -- whose length has to match the arity of the function in the functional pattern.
-myLast :: (Argument [a], Result [a]) => [a] -> a
+myLast :: (Argument a, Result a) => [a] -> a
 myLast $(funPat 'myAppend [[p| _ |], [p| [x] |]]) = x
-myLast _                                        = error "myLast: empty list"
+myLast _                                          = error "myLast: empty list"
 -- ghci> myLast [1,2,3]
 -- 3
